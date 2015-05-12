5e989c28edda8b57aa0c9ab6b87047ecbb53e876
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2015-05-12 23:01:21 +0900

    スパイのサンプルを追加。

--- src/test/java/SocketClientTest.java
+++ src/test/java/SocketClientTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.fail;
 import java.io.IOException;
 import java.net.Socket;
 
+import mockit.Invocation;
 import mockit.Mock;
 import mockit.MockUp;
 
@@ -50,6 +51,30 @@ public class SocketClientTest {
 			fail();
 		} catch(IOException e) {
 			// 例外がスローされることを期待
+		}
+	}
+
+	@Test
+	public void testCloseSpy() {
+		// 記録フェーズ
+		String expectedHost = "www.yahoo.co.jp";
+		int expectedPort = 80;
+		new MockUp<Socket>() {
+			@Mock
+			void close(Invocation invocation) throws IOException {
+				// 引数をコンソールに出力
+				System.out.println("close method invoked.");
+				// 本物のメソッドを呼ぶ
+				invocation.proceed();
+			}
+		};
+		
+		// リプレイフェーズ
+		try {
+			Socket s = new SocketClient().connect(expectedHost, expectedPort);
+			s.close();
+		} catch(IOException e) {
+			fail();
 		}
 	}
 }
